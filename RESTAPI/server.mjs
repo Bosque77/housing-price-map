c12a5a170c5b25b4136c8ccaf69f834d37f346ca
SimranSingh Bapla <baplas@flip1.engr.oregonstate.edu>
2023-02-27 18:39:05 -0800

    implemented routes for Homes, still need to test and merge with front end. Also, should add code to check for invalid requests

--- RESTAPI/server.mjs
+++ RESTAPI/server.mjs
@@ -1,5 +1,3 @@
-
-import * as model from './model.mjs';
 import express from 'express';
 import asyncHandler from 'express-async-handler';
 import * as db from './dbcon.mjs';
@@ -13,14 +11,32 @@ const PORT = 9178;
 app.use(express.json());
 
 /*
+    ROUTE to diplay all cities in select drop down menu
+*/
+app.get('/dropdown_cities', asyncHandler (async (req, res) => {
+
+    let q = 'SELECT city_name FROM Cities;';
+
+    db.pool.query(q, (err, result) => {
+
+        if (err) {
+            console.log(err);
+        }
+        else {
+            res.status(200).send(JSON.stringify(result))
+        }
+
+
+    })
+
+}));
+
+/*
     ROUTES for Homes
 */
-
-// select rows based on city_id
 app.get('/Homes', asyncHandler (async (req, res) => {
 
     let city_id = req.query.cityid.toString();
-    console.log(city_id)
 
     let q = `SELECT Homes.home_id, Homes.street, Homes.sq_ft, Homes.num_of_bed, Homes.num_of_bath FROM Homes WHERE Homes.city_id=${city_id};`;
 
@@ -37,10 +53,62 @@ app.get('/Homes', asyncHandler (async (req, res) => {
 
 app.post('/Homes', asyncHandler (async (req, res) => {
 
+    let q = `INSERT INTO Homes (street, sq_ft, num_of_bed, num_of_bath, year_built, lat, lng, zip, city_id) \
+    VALUES(${req.body.street}, ${req.body.sq_ft}, ${req.body.num_of_bed}, ${req.body.num_of_bath}, ${req.body.year_built}, ${req.body.lat}, ${req.body.lng}, :${req.body.zip}, (SELECT Cities.city_id FROM Cities WHERE city_name = ${req.body.city_name}))`;
     
-}))
+    db.pool.query(q, (err, result) => {
+        if (err) {
+            console.log(err);
+        }
+        else {
+            res.status(204).send(JSON.stringify(result))
+        }
+    }) 
 
-//
+}));
+
+app.put('/Homes', asyncHandler (async (req, res) => {
+
+    let q = `UPDATE Homes \
+    SET street = ${req.body.street} \
+        sq_ft = ${req.body.sq_ft}, \
+        num_of_bed = ${req.body.num_of_bed}, \
+        num_of_bath = ${req.body.num_of_bath}, \
+        year_built = ${req.body.year_built}, \
+        lat = ${req.body.lat}, \
+        lng = ${req.body.lng}, \
+        zip = ${req.body.zip}, \
+        city_id = (SELECT Cities.city_id FROM Cities WHERE city_name = ${req.body.city_name}) \
+    WHERE home_id = ${req.body.home_id};`;
+    
+    db.pool.query(q, (err, result) => {
+        if (err) {
+            console.log(err);
+        }
+        else {
+            res.status(200).send(JSON.stringify(result))
+        }
+    }); 
+
+}));
+
+app.delete('/Homes', asyncHandler (async (req, res) => {
+
+    let q = `DELETE FROM Homes \
+    WHERE Homes.home_id = ${req.body.home_id};`;
+    
+    db.pool.query(q, (err, result) => {
+        if (err) {
+            console.log(err);
+        }
+        else {
+            res.status(204).send(JSON.stringify(result))
+        }
+    }); 
+
+}));
+
+
 
 /*
     LISTENER
