c35c3550b45a78908ad52ba1bc79fe198e7dcf95
Forest <forestschwrtz@gmail.com>
2023-02-28 21:29:36 -0800

    added cors to server and got read working for houses

--- RESTAPI/server.mjs
+++ RESTAPI/server.mjs
@@ -1,74 +1,77 @@
-import express from 'express';
-import asyncHandler from 'express-async-handler';
-import * as db from './dbcon.mjs';
-
+import express from "express";
+import asyncHandler from "express-async-handler";
+import cors from 'cors'
+import * as db from "./dbcon.mjs";
 
 // Express
 
 let app = express();
 const PORT = 9178;
 
+app.use(cors());
+
 app.use(express.json());
 
 /*
     ROUTE to diplay all cities in select drop down menu
 */
-app.get('/dropdown_cities', asyncHandler (async (req, res) => {
-
-    let q = 'SELECT city_name FROM Cities;';
+app.get(
+  "/dropdown_cities",
+  asyncHandler(async (req, res) => {
+    let q = "SELECT city_id, city_name FROM Cities;";
 
     db.pool.query(q, (err, result) => {
-
-        if (err) {
-            console.log(err);
-        }
-        else {
-            res.status(200).send(JSON.stringify(result))
-        }
-
-
-    })
-
-}));
+      if (err) {
+        console.log(err);
+      } else {
+        res.status(200).send(JSON.stringify(result));
+      }
+    });
+  })
+);
 
 /*
     ROUTES for Homes
 */
-app.get('/Homes', asyncHandler (async (req, res) => {
+app.get(
+  "/Homes",
+  asyncHandler(async (req, res) => {
 
+    console.log('inside get Homes')
     let city_id = req.query.cityid.toString();
 
     let q = `SELECT Homes.home_id, Homes.street, Homes.sq_ft, Homes.num_of_bed, Homes.num_of_bath FROM Homes WHERE Homes.city_id=${city_id};`;
 
     db.pool.query(q, (err, result) => {
-        if (err) {
-            console.log(err);
-        }
-        else {
-            res.status(200).send(JSON.stringify(result))
-        }
-    })  
-  
-}));
+      if (err) {
+        console.log(err);
+      } else {
+        console.log(result)
+        res.status(200).send(JSON.stringify(result));
+      }
+    });
+  })
+);
 
-app.post('/Homes', asyncHandler (async (req, res) => {
-
+app.post(
+  "/Homes",
+  asyncHandler(async (req, res) => {
     let q = `INSERT INTO Homes (street, sq_ft, num_of_bed, num_of_bath, year_built, lat, lng, zip, city_id) \
     VALUES(${req.body.street}, ${req.body.sq_ft}, ${req.body.num_of_bed}, ${req.body.num_of_bath}, ${req.body.year_built}, ${req.body.lat}, ${req.body.lng}, :${req.body.zip}, (SELECT Cities.city_id FROM Cities WHERE city_name = ${req.body.city_name}))`;
-    
+
     db.pool.query(q, (err, result) => {
-        if (err) {
-            console.log(err);
-        }
-        else {
-            res.status(204).send(JSON.stringify(result))
-        }
-    }) 
+      if (err) {
+        console.log(err);
+      } else {
+        res.status(204).send(JSON.stringify(result));
+      }
+    });
+  })
+);
 
-}));
-
-app.put('/Homes', asyncHandler (async (req, res) => {
-
+app.put(
+  "/Homes",
+  asyncHandler(async (req, res) => {
     let q = `UPDATE Homes \
     SET street = ${req.body.street} \
         sq_ft = ${req.body.sq_ft}, \
@@ -80,39 +83,44 @@ app.put('/Homes', asyncHandler (async (req, res) => {
         zip = ${req.body.zip}, \
         city_id = (SELECT Cities.city_id FROM Cities WHERE city_name = ${req.body.city_name}) \
     WHERE home_id = ${req.body.home_id};`;
-    
+
     db.pool.query(q, (err, result) => {
-        if (err) {
-            console.log(err);
-        }
-        else {
-            res.status(200).send(JSON.stringify(result))
-        }
-    }); 
+      if (err) {
+        console.log(err);
+      } else {
+        res.status(200).send(JSON.stringify(result));
+      }
+    });
+  })
+);
 
-}));
-
-app.delete('/Homes', asyncHandler (async (req, res) => {
-
+app.delete(
+  "/Homes",
+  asyncHandler(async (req, res) => {
     let q = `DELETE FROM Homes \
     WHERE Homes.home_id = ${req.body.home_id};`;
-    
+
     db.pool.query(q, (err, result) => {
-        if (err) {
-            console.log(err);
-        }
-        else {
-            res.status(204).send(JSON.stringify(result))
-        }
-    }); 
+      if (err) {
+        console.log(err);
+      } else {
+        res.status(204).send(JSON.stringify(result));
+      }
+    });
+  })
+);
 
-}));
-
-
+app.get('/*', (req, res) => {
+  res.send("hello; this is the REST API server");
+});
 
 /*
     LISTENER
 */
-app.listen(PORT, function(){
-    console.log('Express started on http://localhost:' + PORT + '; press Ctrl-C to terminate.')
+app.listen(PORT, function () {
+  console.log(
+    "Express started on http://localhost:" +
+      PORT +
+      "; press Ctrl-C to terminate."
+  );
 });
--- client-server/src/pages/admin/houses.tsx
+++ client-server/src/pages/admin/houses.tsx
@@ -5,19 +5,46 @@ import { House } from "types";
 import CreateHouse from "@/components/CreateHouse";
 import DeleteModal from "@/components/DeleteModal";
 
-import { cities, homes } from "@/data/ex_data.js";
-import { useState } from "react";
+// import { cities, homes } from "@/data/ex_data.js";
+import { useEffect, useState } from "react";
 
-const HousesPage = () => {
+const cities_url = "http://localhost:9178/dropdown_cities";
 
+export async function getServerSideProps() {
+  console.log(cities_url)
+  const res = await fetch(cities_url);
+  const cities = await res.json();
 
+  const city_id = cities[0].city_id
+  const query_data = {
+    cityid: city_id,
+  };
+
+  console.log(query_data)
+
+  const queryParams = new URLSearchParams(query_data).toString();
+  const homes_url = `http://localhost:9178/Homes?${queryParams}`
+  console.log(homes_url)
+  const res_2 = await fetch(homes_url);
+  const homes = await res_2.json()
+  console.log(homes)
+
+  // Return data as props...
+  return { props: { cities, homes } };
+}
+
+interface prop {
+  cities: any;
+  homes: any;
+}
+
+const HousesPage = ({cities,homes}: prop) => {
 
   const [showCreateHouse, setShowCreateHouse] = useState(false);
   const [currentHouse, setCurrentHouse] = useState(undefined);
-  const [showDeleteModal, setShowDeleteModal] = useState(false)
-  const [shouldDelete, setShouldDelete] = useState(false)
-  const [should_delete_object_name, setShouldDeleteObjectName] = useState("")
-
+  const [showDeleteModal, setShowDeleteModal] = useState(false);
+  const [shouldDelete, setShouldDelete] = useState(false);
+  const [should_delete_object_name, setShouldDeleteObjectName] = useState("");
 
   return (
     <div className="bg-gray-50 h-screen">
@@ -25,45 +52,70 @@ const HousesPage = () => {
       <div className="flex flex-row h-screen ">
         <SideNav />
         <div className="flex flex-row justify-center w-full bg-gray-50">
-          <HousesComponent setShowCreateHouse = {setShowCreateHouse} setCurrentHouse={setCurrentHouse} setShowDeleteModal={setShowDeleteModal}/>
+          <HousesComponent
+            setShowCreateHouse={setShowCreateHouse}
+            setCurrentHouse={setCurrentHouse}
+            setShowDeleteModal={setShowDeleteModal}
+            cities={cities}
+            homes={homes}
+          />
         </div>
-        {showCreateHouse && <CreateHouse setShowCreateHouse = {setShowCreateHouse} currentHouse={currentHouse}/>}
-        {showDeleteModal && <DeleteModal setShowDeleteModal = {setShowDeleteModal} setShouldDelete={setShouldDelete}/>}
-
+        {showCreateHouse && (
+          <CreateHouse
+            setShowCreateHouse={setShowCreateHouse}
+            currentHouse={currentHouse}
+          />
+        )}
+        {showDeleteModal && (
+          <DeleteModal
+            setShowDeleteModal={setShowDeleteModal}
+            setShouldDelete={setShouldDelete}
+          />
+        )}
       </div>
-      
-     
     </div>
   );
 };
 
 interface prop {
-  setShowCreateHouse: React.Dispatch<React.SetStateAction<boolean>>,
-  setShowDeleteModal: React.Dispatch<React.SetStateAction<boolean>>,
-
-  setCurrentHouse: React.Dispatch<React.SetStateAction<any>>,
+  setShowCreateHouse: React.Dispatch<React.SetStateAction<boolean>>;
+  setShowDeleteModal: React.Dispatch<React.SetStateAction<boolean>>;
+  cities: any;
+  homes: any;
+  setCurrentHouse: React.Dispatch<React.SetStateAction<any>>;
 }
 
-const HousesComponent = ({setShowCreateHouse, setCurrentHouse, setShowDeleteModal}:prop) => {
+const HousesComponent = ({
+  setShowCreateHouse,
+  setCurrentHouse,
+  setShowDeleteModal,
+  cities,
+  homes,
+}: prop) => {
 
 
 
-  const onShowHouse = ()  => {
-    console.log('on show house')
-    setShowCreateHouse(true)
-  }
+  const onShowHouse = () => {
+    console.log("on show house");
+    setShowCreateHouse(true);
+  };
 
   const editHouse = (house: House) => {
-    console.log('inside editHouse')
-    console.log(house)
-    setCurrentHouse(house)
-    setShowCreateHouse(true)
-  }
+    console.log("inside editHouse");
+    console.log(house);
+    setCurrentHouse(house);
+    setShowCreateHouse(true);
+  };
 
   const onDelete = (house: House) => {
-    setShowDeleteModal(true)
-  }
+    setShowDeleteModal(true);
+  };
 
+  const insertCities = () => {
+    return cities.map((city: any) => (
+      <option value={city.city_id}>{city.city_name}</option>
+    ));
+  };
 
   return (
     <div>
@@ -73,9 +125,7 @@ const HousesComponent = ({setShowCreateHouse, setCurrentHouse, setShowDeleteModa
             <div className="flex mb-6">
               <label className="mr-3 content-center text-lg">Select City</label>
               <select className="border px-4 bg-white text-lg">
-                <option value="1">San Francisco</option>
-                <option value="2">Los Angelos</option>
-                <option value="3">Berkley</option>
+                {insertCities()}
               </select>
             </div>
 
@@ -92,27 +142,43 @@ const HousesComponent = ({setShowCreateHouse, setCurrentHouse, setShowDeleteModa
                   </tr>
                 </thead>
                 <tbody>
-                  {homes.map((home) => (
+                  {homes.map((home: House) => (
                     <tr key={home.home_id}>
                       <td className="border px-4 py-2">{home.home_id}</td>
                       <td className="border px-4 py-2">{home.street}</td>
                       <td className="border px-4 py-2">{home.sq_ft}</td>
                       <td className="border px-4 py-2">{home.num_of_bed}</td>
                       <td className="border px-4 py-2">{home.num_of_bath}</td>
-                      <td className="px-4 py-2 " ><button className="hover:underline hover:underline-offset-1" onClick={() => editHouse(home)}>Edit</button></td>
-                      <td className="px-4 py-2"><button className="hover:underline hover:underline-offset-1" onClick = {() => onDelete(home)}>Delete</button></td>
+                      <td className="px-4 py-2 ">
+                        <button
+                          className="hover:underline hover:underline-offset-1"
+                          onClick={() => editHouse(home)}
+                        >
+                          Edit
+                        </button>
+                      </td>
+                      <td className="px-4 py-2">
+                        <button
+                          className="hover:underline hover:underline-offset-1"
+                          onClick={() => onDelete(home)}
+                        >
+                          Delete
+                        </button>
+                      </td>
                     </tr>
                   ))}
                 </tbody>
               </table>
-              <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-4" onClick={() => onShowHouse()}>
+              <button
+                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-4"
+                onClick={() => onShowHouse()}
+              >
                 Add House
               </button>
             </div>
           </div>
         </div>
       </div>
-
     </div>
   );
 };
