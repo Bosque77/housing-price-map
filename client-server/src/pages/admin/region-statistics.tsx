7584ec989d1909d9b1d1f9e78bd21a4d06209d56
SimranSingh Bapla <baplas@flip2.engr.oregonstate.edu>
2023-03-18 23:35:38 -0700

    changed left old code commented

--- client-server/src/pages/admin/region-statistics.tsx
+++ client-server/src/pages/admin/region-statistics.tsx
@@ -84,3 +84,132 @@
 // export default RegionStatisticsPage;
 
 
+// import { useState, useEffect, ChangeEvent } from "react";
+// import axios from "axios";
+// import Header from "@/components/Header";
+// import SideNav from "@/components/SidNav";
+
+// interface RegionStatistic {
+//   region_statistic_id: number;
+//   // Add other properties of the region statistic object
+// }
+
+// interface RegionStatisticsProps {
+//   regionStats: RegionStatistic[];
+//   selectedRegion: string;
+//   onRegionChange: (e: ChangeEvent<HTMLSelectElement>) => void;
+//   onDeleteRegionStatistic: (region_statistic_id: number) => void;
+// }
+
+// const RegionStatisticsPage = () => {
+//   const [regionStats, setRegionStats] = useState<RegionStatistic[]>([]);
+//   const [selectedRegion, setSelectedRegion] = useState<string>("");
+
+//   useEffect(() => {
+//     loadRegionStatistics();
+//   }, []);
+
+//   const loadRegionStatistics = async () => {
+//     const response = await axios.get<RegionStatistic[]>("/api/region_statistics");
+//     setRegionStats(response.data);
+//   };
+
+//   const deleteRegionStatistic = async (region_statistic_id: number) => {
+//     await axios.delete(`/api/region_statistics/${region_statistic_id}`);
+//     loadRegionStatistics();
+//   };
+
+//   const handleRegionChange = (e: ChangeEvent<HTMLSelectElement>) => {
+//     setSelectedRegion(e.target.value);
+//   };
+
+//   return (
+//     <div className="bg-gray-50 h-screen">
+//       <Header />
+//       <div className="flex flex-row h-screen ">
+//         <SideNav />
+//         <div className="flex flex-row justify-center w-full bg-gray-50">
+//           <RegionStatistics
+//             regionStats={regionStats}
+//             selectedRegion={selectedRegion}
+//             onRegionChange={handleRegionChange}
+//             onDeleteRegionStatistic={deleteRegionStatistic}
+//           />
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
+
+// const RegionStatistics: React.FC<RegionStatisticsProps> = ({
+//   regionStats,
+//   selectedRegion,
+//   onRegionChange,
+//   onDeleteRegionStatistic,
+// }) => {
+//   return (
+//     <div>
+//       <table className="table-auto">
+//         <thead>
+//           <tr>
+//             <th>Region Statistic ID</th>
+//             {/* Add other table header cells for each property */}
+//             <th>Edit</th>
+//             <th>Delete</th>
+//           </tr>
+//         </thead>
+//         <tbody>
+//           {regionStats.map((stat) => (
+//             <tr key={stat.region_statistic_id}>
+//               <td>{stat.region_statistic_id}</td>
+//               {/* Render table cells for each property */}
+//               <td className="px-4">Edit</td>
+//               <td>
+//                 <button
+//                   onClick={() => onDeleteRegionStatistic(stat.region_statistic_id)}
+//                 >
+//                   Delete
+//                 </button>
+//               </td>
+//             </tr>
+//           ))}
+//         </tbody>
+//       </table>
+
+//       <select
+//         value={selectedRegion}
+//         onChange={onRegionChange}
+//         className="border px-4 py-2 bg-white text-lg mb-8"
+//       >
+//         <option value="1">Bay Area</option>
+//         <option value="2">Los Angeles Metro</option>
+//       </select>
+//     </div>
+//   );
+// };
+
+// export default RegionStatisticsPage;
+
+
+import Header from "@/components/Header";
+import SideNav from "@/components/SidNav";
+import RegionStatisticModal from "@/components/RegionStatisticModal";
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+const RegionStatisticsPage = () => {
+  // ...
+
+  return (
+    <div className="bg-gray-50 h-screen">
+      <Header />
+      <div className="flex flex-row h-screen ">
+        <SideNav />
+        <div className="flex flex-row justify-center w-full bg-gray-50">
+          <RegionStatistics />
+        </div>
+      </div>
+    </div>
+  );
+};
+
